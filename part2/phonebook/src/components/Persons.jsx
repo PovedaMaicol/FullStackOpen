e45a16392ae935877e3aefa2845f1f3c9408d421
Maicol Salazar <stivsal12@gmail.com>
2024-05-22 22:48:35 -0500

    advances of styles

--- part2/phonebook/src/components/Persons.jsx
+++ part2/phonebook/src/components/Persons.jsx
@@ -6,7 +6,22 @@ const Persons = ({coincidences, deletePerson}) => {
     <ul className='grid-container'>
       {
         coincidences.map(person => 
-<li key={person.id}>{person.name} {person.number} <button onClick={() => deletePerson(person.id)}>delete</button></li>
+<li key={person.id}>
+  <div>
+<i onClick={() => deletePerson(person.id)} className='bx bx-x borrar' ></i>
+</div>
+
+  <h1 className='identificador'>{person.name[0]}</h1>
+    
+  <div className='contact-info'> 
+  <span className='contact-name'>{person.name}</span> 
+  <span className='contact-number'>{person.number}</span>
+   </div>
+ 
+  
+
+  
+  </li>
 
         
           
