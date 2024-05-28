54c14e2329881d07eba71eca4acb2cf1aa5571fe
Maicol Salazar <stivsal12@gmail.com>
2024-05-27 19:25:59 -0500

    advances backend HTTP request

--- part3/backendPhonebook/index.js
+++ part3/backendPhonebook/index.js
@@ -19,16 +19,81 @@ let persons = [
     }
 ]
 
-
+app.get('/info', (request, response) => {
+    const now = new Date();
+    response.send(`<div>
+    <h1>Phonebook has info for ${persons.length} people</h1>
+    <br/>
+    ${now}
+    </div>`)
+})
 //GET ALL
 app.get('/api/persons', (request, response) => {
     response.json(persons)
 })
 
 //GET ONE
-app.get('/api/contacts/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+   
+
+    const person = persons.find(person => person.id === id)
     
+
+    if(person){
+        response.json(person)
+    } else {
+        response.statusMessage = "Current id does not match"
+        response.status(404).end()
+    }
 })
+
+// Delete
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    console.log(id)
+    persons = persons.filter(person => person.id !== id)
+    console.log(persons)
+
+    response.status(204).end()
+})
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+// POST
+app.post('/api/persons', (request, response) => {
+    const body = request.body;
+    const normalize = (text) => text.toUpperCase().trim().replace(/\s+/g, '');
+console.log('lo que ahi en body es', body)
+    if (!body.name || !body.number ) {
+        return response.status(400).json({
+            error: 'content missing (name and number required)'
+        });
+    }
+
+    const nameRepeat = persons.find(person => normalize(person.name) === normalize(body.name))
+
+    if(nameRepeat) {
+        return response.status(400).json({
+            error: 'name must be unique'
+        });
+    }
+
+    
+    const person = {
+        name: body.name,
+        number: body.number,
+        gmail: body.gmail,
+        birthday: body.birthday,
+        id: getRandomInt(5000)
+    
+    };
+
+    persons = persons.concat(person);
+
+    response.json(person);
+});
 
 
 const PORT = 3001
