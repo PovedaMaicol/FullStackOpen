9f1164a8b294d1650953c710ba17574d5f452b8e
Maicol Salazar <stivsal12@gmail.com>
2024-05-29 21:04:58 -0500

    advances mongo database

--- part3/courseinfo/index.js
+++ part3/courseinfo/index.js
@@ -1,89 +1,69 @@
+require('dotenv').config();
 const express = require('express') // importamos express
-const app = express()
-// importo y configuro cors
-// cors permite solicitudes de otros origenes
 const cors = require('cors')
-app.use(cors())
+const Note = require('./models/note')
 
-app.use(express.static('dist'))
+
+const app = express()
+
+
+// middlewares
+
+app.use(cors())
+// app.use(express.static('dist'))
 app.use(express.json())
-let notes = [
-    {
-      id: 1,
-      content: "HTML is easy",
-      important: true,
-      date: new Date()
-    },
-    {
-      id: 2,
-      content: "Browser can execute only JavaScript",
-      important: false
-    },
-    {
-      id: 3,
-      content: "GET and POST are the most important methods of HTTP protocol",
-      important: true
-    }
-  ]
+
+// HOME
   app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
   })
   
-  // GET ALL
+  // GET ALL - MONGO
   app.get('/api/notes', (request, response) => {
-    response.json(notes)
+    Note.find({}).then(notes => {
+      response.json(notes)
+    })
   })
-  // GET ONE
-  app.get('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const note = notes.find(note => note.id === id)
 
-    if (note) {
-      response.json(note)
-    }else{
-      // si note es undefined... entonces 
-      response.statusMessage = "Current id does not match"
-      response.status(404).end()
-    }
-   
+
+  // GET ONE - con el metodo findById
+  app.get('/api/notes/:id', (request, response) => {
+Note.findById(request.params.id).then(note => {
+  response.json(note)
+})
   })
   
   // DELETE
-  app.delete('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    notes = notes.filter(note => note.id !== id)
+  // app.delete('/api/notes/:id', (request, response) => {
+  //   const id = Number(request.params.id)
+  //   notes = notes.filter(note => note.id !== id)
   
-    response.status(204).end()
-  })
+  //   response.status(204).end()
+  // })
 
-  //POST
-  const generateId = () => {
-    const maxId = notes.length > 0
-      ? Math.max(...notes.map(n => n.id))
-      : 0
-    return maxId + 1
-  }
-  
-app.post('/api/notes', (request, response) => {
-  const body = request.body
 
-  if (!body.content) {
-    return response.status(400).json({ 
-      error: 'content missing' 
+    //POST MONGO
+    app.post('/api/notes', (request, response) => {
+      const body = request.body;
+    
+      if (!body.content) {
+        return response.status(400).json({ error: 'content missing' })
+      }
+    
+      const note = new Note({
+        content: body.content,
+        important: body.important || false,
+      })
+    
+      note.save().then(savedNote => {
+        response.json(savedNote)
+      })
     })
-  }
+// Middleware para manejar rutas desconocidas
+app.use((request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' });
+});
 
-  const note = {
-    content: body.content,
-    important: Boolean(body.important) || false,
-    id: generateId(),
-  }
-
-  notes = notes.concat(note)
-
-  response.json(note)
-})
-
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
