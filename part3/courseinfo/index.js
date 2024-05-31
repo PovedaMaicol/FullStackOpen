9161989c892db5254d03628b2c7d568cbc7f5e4c
Maicol Salazar <stivsal12@gmail.com>
2024-05-30 23:07:17 -0500

    GET ALL, GET ONE, UPDATE AND DELETE THEY ARE COMPLETE

--- part3/courseinfo/index.js
+++ part3/courseinfo/index.js
@@ -2,16 +2,18 @@ require('dotenv').config();
 const express = require('express') // importamos express
 const cors = require('cors')
 const Note = require('./models/note')
-
-
 const app = express()
 
 
 // middlewares
 
 app.use(cors())
-// app.use(express.static('dist'))
+app.use(express.static('dist'))
 app.use(express.json())
+
+
+
+
 
 // HOME
   app.get('/', (request, response) => {
@@ -25,44 +27,90 @@ app.use(express.json())
     })
   })
 
-
-  // GET ONE - con el metodo findById
-  app.get('/api/notes/:id', (request, response) => {
-Note.findById(request.params.id).then(note => {
-  response.json(note)
+  // GET ONE - MONGO con el metodo findById
+  app.get('/api/notes/:id', (request, response, next) => {
+Note.findById(request.params.id)
+.then(note => {
+  if(note) {
+    response.json(note)
+  } else {
+    response.status(404).end()
+  }
 })
+.catch(error => next(error))
+  })
+
+  //POST MONGO
+  app.post('/api/notes', (request, response) => {
+  const body = request.body;
+      
+  if (!body.content) {
+  return response.status(400).json({ error: 'content missing' })
+  }
+      
+  const note = new Note({
+  content: body.content,
+  important: body.important || false,
+  })
+      
+  note.save().then(savedNote => {
+  response.json(savedNote)
+  })
   })
   
-  // DELETE
-  // app.delete('/api/notes/:id', (request, response) => {
-  //   const id = Number(request.params.id)
-  //   notes = notes.filter(note => note.id !== id)
-  
-  //   response.status(204).end()
-  // })
+  // UPDATE - MONGO metodo findByIdAndUpdate
+  app.put('/api/notes/:id', (request, response, next) => {
+    const body = request.body 
 
+    const note = {
+      content: body.content,
+      important: body.important
+    }
 
-    //POST MONGO
-    app.post('/api/notes', (request, response) => {
-      const body = request.body;
-    
-      if (!body.content) {
-        return response.status(400).json({ error: 'content missing' })
-      }
-    
-      const note = new Note({
-        content: body.content,
-        important: body.important || false,
-      })
-    
-      note.save().then(savedNote => {
-        response.json(savedNote)
-      })
+    Note.findByIdAndUpdate(request.params
+      .id, note, { new: true }
+    )
+    .then(updatedNote => {
+      response.json(updatedNote)
     })
-// Middleware para manejar rutas desconocidas
-app.use((request, response) => {
+    .catch(error => next(error))
+  })
+
+  
+  // DELETE - MONGO con el metodo findByIdAndDelete
+  app.delete('/api/notes/:id', (request, response, next) => {
+    Note.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+  })
+
+
+
+
+
+
+// Middleware para manejar rutas desconocida
+const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' });
-});
+}
+
+app.use(unknownEndpoint)
+
+// Middleware para manejo de errores
+const errorHandler = (error, request, response, next) => {
+  console.log(error.message)
+
+  if(error.name === 'CastError') {
+    return response.status(400).send({error: 'malformatted id'})
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
+
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
