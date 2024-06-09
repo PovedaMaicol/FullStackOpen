18dd6ff2d39221a40b791815d6be854ae505cf19
Maicol Salazar <stivsal12@gmail.com>
2024-06-09 13:01:34 -0500

    start refactoring

--- part4/index.js
+++ part4/index.js
@@ -1,31 +1,42 @@
-// const express = require('express')
-// const app = express()
-// const cors = require('cors')
-// const mongoose = require('mongoose')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
 
-
-
-// const Blog = mongoose.model('Blog', blogSchema)
-
-// const mongoUrl = 'mongodb://localhost/bloglist'
-// mongoose.connect(mongoUrl)
-
-// app.use(cors())
-// app.use(express.json())
-
-
-
-// app.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`)
-// })
-
-const http = require('http')
-
-const app = http.createServer((request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/plain' })
-  response.end('Hello World')
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
 })
 
-const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+const Blog = mongoose.model('Blog', blogSchema)
+
+const mongoUrl = 'mongodb+srv://backendp:12345ba@cluster0.qgjpa3f.mongodb.net/blogsApp?retryWrites=true&w=majority'
+mongoose.connect(mongoUrl)
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/api/blogs', (request, response) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
+})
+
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
+})
+
+const PORT = 3003
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
