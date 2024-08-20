438765d9d5ff9963d59005bbe8b65114da781cf6
Maicol Salazar <stivsal12@gmail.com>
2024-08-19 19:20:54 -0500

    advances React Qyery

--- part6/query-notes/services/request.js
+++ part6/query-notes/services/request.js
@@ -2,3 +2,6 @@ import axios from 'axios'
 
 export const getNotes = () =>
   axios.get('http://localhost:3001/notes').then(res => res.data)
+
+export const createNote = newNote =>
+  axios.post(baseUrl, newNote).then(res => res.data)
--- part6/query-notes/src/App.jsx
+++ part6/query-notes/src/App.jsx
@@ -1,9 +1,23 @@
-import { useQuery } from "@tanstack/react-query"
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query"
 import axios from "axios"
-import { getNotes } from "../services/request"
+import { createNote, getNotes } from "../services/request"
 
 
 const App = () => {
+
+  const queryClient = useQueryClient()
+  // useQueryClient() es un hook de React Query que proporciona acceso al QueryClient, el cual es la instancia que maneja la cache de todas las consultas (queries) y mutaciones (mutations) en tu aplicación.
+
+  const newNoteMutation = useMutation({
+  
+  //useMutation es un hook de React Query usado para realizar mutaciones, es decir, para realizar operaciones que cambian los datos en el servidor (como crear, actualizar o eliminar un recurso).
+
+    mutationFn: createNote,
+    onSuccess: (newNote) => {
+      const notes = queryClient.getQueryData(['notes'])
+      queryClient.setQueryData(['notes'], notes.concat(newNote))
+    }
+  })
   const addNote = async (event) => {
     event.preventDefault()
     const content = event.target.note.value
@@ -17,7 +31,8 @@ const App = () => {
 
   const result = useQuery({
     queryKey: ['notes'],
-    queryFn: getNotes
+    queryFn: getNotes,
+    refetchOnWindowFocus: false
   })
   console.log(JSON.parse(JSON.stringify(result)))
 
