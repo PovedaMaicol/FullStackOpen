32777effb606ff0614e542a5c27ee85a708b0ff3
Maicol Salazar <stivsal12@gmail.com>
2024-10-28 17:07:29 -0500

    enum and Type Guards

--- part9/DiarioVuelos/src/routes/diaries.ts
+++ part9/DiarioVuelos/src/routes/diaries.ts
@@ -1,14 +1,48 @@
 import express from 'express';
 import diaryService from '../services/diaryService';
+import { NewDiaryEntry } from '../types';
+import toNewDiaryEntry from '../utils';
 
 const router = express.Router();
+
+router.get('/:id', (_req, res) => {
+  const diary = diaryService.findById(Number(_req.params.id));
+
+  if (diary) {
+  res.send(diary);
+  } else {
+  res.sendStatus(404);
+  }
+})
 
 router.get('/', (_req, res) => {
   res.send(diaryService.getNonSensitiveEntries());
 })
 
 router.post('/', (_req, res) => {
-  res.send('Saving a diary!');
+  try {
+    const newDiaryEntry = toNewDiaryEntry(_req.body);
+
+    const addedEntry = diaryService.addDiary(newDiaryEntry);
+    res.json(addedEntry);
+  } catch (error: unknown) {
+    let errorMessage = 'Something went wrong.';
+    if(error instanceof Error) {
+      errorMessage += ' Error: ' + error.message;
+    }
+    res.status(400).send(errorMessage);
+  }
+  const { date, weather, visibility, comment } = _req.body;
+  const newEntry: NewDiaryEntry = {
+    date,
+    weather,
+    visibility,
+    comment,
+  };
+  const addedEntry = diaryService.addDiary(newEntry);
+  res.json(addedEntry);
 })
+
+
 
 export default router;
