a351795f332941d7632691b2fb1d0d76ca09ae2f
Maicol Salazar <stivsal12@gmail.com>
2024-11-29 17:44:35 -0500

    advances

--- part10/rate-repository-app/App.js
+++ part10/rate-repository-app/App.js
@@ -1,13 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import Main from './components/Main';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  console.log('hola android')
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Main/>
   );
 }
 
