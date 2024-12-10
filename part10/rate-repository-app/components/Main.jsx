21c8a0f2589e99c4d06da1a24a0849f299cf7438
Maicol Salazar <stivsal12@gmail.com>
2024-12-10 18:34:43 -0500

    routes

--- part10/rate-repository-app/components/Main.jsx
+++ part10/rate-repository-app/components/Main.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
-import Constants from 'expo-constants';
+// import Constants from 'expo-constants';
 import { Text, StyleSheet, View } from 'react-native';
-import RepositoryList from './RepositoryList';
-import AppBar from './AppBar';
-import { TouchableWithoutFeedback } from 'react-native-web';
 
+import { Route, Routes, Navigate } from 'react-router-native';
+
+import RepositoryList  from './RepositoryList';
+import AppBar from './AppBar';
+import theme from '../src/theme'
+
+
+
+console.log('Repository list:', RepositoryList)
+console.log('AppBar', AppBar)
+console.log('theme', theme)
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: theme.colors.primary,
     flexGrow: 1,
     flexShrink: 1,
   },
@@ -15,13 +24,17 @@ const styles = StyleSheet.create({
 const Main = () => {
   return (
     <View style={styles.container}>
+      <AppBar/>
+      <Routes>
+        <Route path='/' element={<RepositoryList/>}/>
+        <Route path='*' element={<Navigate to='/' replace />}/>
+        
+      </Routes>
 
       
-      <AppBar/>
-     
-
-      <Text>Rate Repository Application</Text>
-      <RepositoryList/>
+  
+    
+    
     </View>
   );
 };
